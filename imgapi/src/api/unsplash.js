452dd5a8ea7ceaf956a5b43b0b099326519e4118
Adam Smith <38594717+asmitheyexe@users.noreply.github.com>
2019-11-18 13:08:34 -0600

    Update unsplash.js

--- imgapi/src/api/unsplash.js
+++ imgapi/src/api/unsplash.js
@@ -5,7 +5,7 @@ export default axios.create({
     baseURL: 'https://api.unsplash.com', 
     headers:{
         // Access key
-        Authorization: 'Client-ID a59102679b0fa6b516d90446baf4a65d33fbe9e4b9d56e1029d81d6cb89526df'
+        Authorization: 'Client-ID (access token here)'
     }
 
 });
