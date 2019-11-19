97cfe108a7fca66f5b7289f6b4f8b3388060b9a1
Adam Smith <38594717+asmitheyexe@users.noreply.github.com>
2019-11-18 18:29:54 -0600

    Revert "final touches on project"
    This reverts commit 216dc6b35873a9497505f5be0d746412b74367f4.

--- imgapi/src/components/js/ImageList.js
+++ imgapi/src/components/js/ImageList.js
@@ -1,13 +1,14 @@
 import React from 'react';
-import ImageCard from '../js/ImageCard';
-import '../css/ImageList.css';
 
 const ImageList = (props) => {
-    const imagesMapping = props.images.map( (image) =>{
-       return  <ImageCard key={image.id} image={image} /> 
-            
-       });
-    return <div className="image-list">{imagesMapping}</div>;
+    const imagesMapping = props.images.map( ({description, id, urls}) =>{
+       return (
+            <div key={id}>
+                <img alt={description} src={urls.regular} /> 
+            </div>
+       )   
+    });
+    return imagesMapping;
 };
 
 export default ImageList;
