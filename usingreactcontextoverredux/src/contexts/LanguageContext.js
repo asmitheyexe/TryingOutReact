909ebea2c592e290ee1d37bad6dcbf544d7371c5
Adam Smith <38594717+asmitheyexe@users.noreply.github.com>
2019-11-26 19:16:38 -0600

    context kinda sucks

--- usingreactcontextoverredux/src/contexts/LanguageContext.js
+++ usingreactcontextoverredux/src/contexts/LanguageContext.js
@@ -1,4 +1,5 @@
 import React from 'react';
+/*
 // this is all we do to create a context :)
 export default React.createContext('english');
 /*
@@ -13,3 +14,22 @@ React.createContext(5);
 
 All acceptable values, just has to be a valid JavaScript value
 */
+
+const Context = React.createContext('english');
+
+export class LanguageStore extends React.Component{
+    state = {language:'english'};
+    onLanguageChange = (language) =>{
+        this.setState({language});
+    }
+
+    render(){
+        return(
+            <Context.Provider value={{...this.state, onLanguageChange: this.onLanguageChange}} >
+                {this.props.children}
+            </Context.Provider>
+        );
+    }
+}
+
+export default Context;
